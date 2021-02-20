112bd7cc2862d91172257f3364319a2edfc20f9b
jggeddes <72463735+jggeddes@users.noreply.github.com>
2021-02-19 23:14:17 -0600

    Add files via upload

---
+++ Week03/login.php
@@ -0,0 +1,17 @@
+<?php
+
+function saveUser($userid, $firstname, $lastname, $email, $phone, $password) {
+	
+	}
+
+function validateUser($userid, $firstname, $lastname, $email, $phone, $password) {
+	
+	}
+
+function loadUser($userid, $firstname, $lastname, $email, $phone, $password) {
+	
+	}	
+
+//I tried looking for a simpler way to do this but came up empty. 
+//I believe this is what your looking for
+?>
