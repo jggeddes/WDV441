612e2756a38b9f4c0fc3113890fcb5202449e53d
jggeddes <72463735+jggeddes@users.noreply.github.com>
2021-04-15 20:07:47 -0500

    Add files via upload

---
+++ Week11/public/user-rest-list.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+require_once("../inc/users.class.php");
+
+$user = new Users();
+
+$usersList = $user->getListFilteredREST(
+    (isset($_GET['sortColumn']) ? $_GET['sortColumn'] : null),
+    (isset($_GET['sortDirection']) ? $_GET['sortDirection'] : null),
+    (isset($_GET['filterColumn']) ? $_GET['filterColumn'] : null),
+    (isset($_GET['filterText']) ? $_GET['filterText'] : null)
+);
+
+
+echo json_encode($usersList);
+
+?>
